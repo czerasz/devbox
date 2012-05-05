9db750e895e2ee7856a6ac826cb5299a23c9574e
Michael Czeraszkiewicz <czerasz.hosing@gmail.com>
2012-05-06 00:03:41 +0200

    Added rvm with ruby 1.9.3
    Added rvm via the rvm module with support for ruby 1.9.3 and bundler
    
    	modified:   .gitmodules
    	modified:   puppet/modules/boxes/manifests/base.pp
    	new file:   puppet/modules/rvm

--- puppet/modules/boxes/manifests/base.pp
+++ puppet/modules/boxes/manifests/base.pp
@@ -1,10 +1,19 @@
 class boxes::base {
   $version = "0.0.1"
+  
+  #Create the "vagrant" user
+  #this is required for the rvm module
+  user {"vagrant":
+    comment => "Created by Puppet",
+  }
+  group { "vagrant":
+    require => User["vagrant"]
+  }
 
   file {"/etc/motd":
     content => template("boxes/etc/motd.erb"),
     ensure => file,
-    replace => true
+    replace => true,
   }
 
 	class after::system::update {
@@ -12,17 +21,22 @@ class boxes::base {
 
     #install terminal helpers
     package {"tree":
-      ensure => present
+      ensure => present,
     }
 
     #install vim
     package {"vim":
-      ensure => present
+      ensure => present,
     }
     
     #install git
     package {["git-core", "git-doc"]:
-      ensure => present
+      ensure => present,
+    }
+
+    #install curl
+    package {["curl"]:
+      ensure => present,
     }
 
     #-->BEGIN install apache
@@ -41,15 +55,34 @@ class boxes::base {
 
     #-->END install apache
     
+    #-->BEGIN install mySQL
+    #helper:
+    # - https://github.com/geopcgeo/LAMP
+    # - https://github.com/puppetlabs/puppetlabs-mysql
+    #-->END install mySQL
+
+    #-->BEGIN install rvm
+    #from module: https://github.com/jfryman/puppet-rvm
+    include rvm
+    rvm::define::user {"vagrant": }
+    rvm::define::version { 'ruby-1.9.3':
+      ensure => present,
+    }
+    rvm::define::gem {"bundler":
+      ensure => present,
+      ruby_version => "ruby-1.9.3",
+    }
+    #-->END install rvm
+
   }#end class after::system::update
 
   exec { "apt-get update":
-    command => '/usr/bin/apt-get update',
+    command => "/usr/bin/apt-get update",
     before => Notify["Start system update"],
   }
   notify { "Start system update": }
 
   class { "after::system::update":
-    subscribe => Exec["apt-get update"]
+    subscribe => Exec["apt-get update"],
   }
 }
